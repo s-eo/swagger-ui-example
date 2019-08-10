79d60fd68de6a8526c6c4e58c2f0bf6be441fbbb
s-eo <eoskakovskaya@mail.ru>
2019-08-10 20:03:50 +0300

    swagger ui example

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 /**
  * Created by Lena on 19/08/10.
  */
+const express = require('express');
+const http = require('http');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+http.createServer(app).listen('8080', function(){
+    console.log('Express server listening on port 8080');
+});
